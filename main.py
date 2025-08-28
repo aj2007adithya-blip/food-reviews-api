fc8466dbd9ad103dc343ba46844d90457f9134ba
aj2007adithya-blip <aj2007.adithya@gmail.com>
2025-08-28 15:58:26 +0530

    Update main.py to dynamic port

--- main.py
+++ main.py
@@ -19,10 +19,16 @@ def get_reviews(page: int = Query(1, ge=1), limit: int = Query(10, ge=1, le=100)
     start = (page - 1) * limit
     end = start + limit
     paginated_reviews = reviews[start:end]
-    
+
     return {
         "page": page,
         "limit": limit,
         "total_reviews": len(reviews),
         "reviews": paginated_reviews
     }
+
+# Only needed if you want to run locally with `python main.py`
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))  # default to 8000 locally
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
